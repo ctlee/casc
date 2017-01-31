c90c9115f57c72c3d86f0f5ca5801c887991eff2
ctlee <ctlee@ucsd.edu>
2017-01-31 15:54:44 -0800

    Additional cleanup. Now building correctly

--- tests/scratchtest.cpp
+++ tests/scratchtest.cpp
@@ -11,7 +11,6 @@
 
 #include "ASCFunctions.h"
 #include "stringutil.h"
-#include "SurfaceMeshOld.h"
 #include "SurfaceMesh.h"
 #include "SimplicialComplexVisitors.h"
 #include "Vertex.h"
@@ -33,75 +32,6 @@ int  main(int argc, char *argv[])
     else{
         std::cout << "Simplex v doesn't exist" << std::endl;
     }
-    // if(argc != 2)
-    // {
-    //     std::cerr << "Wrong arguments passed" << std::endl;
-    //     return -1;
-    // }
-    // auto mesh = readOBJ(argv[1]);
-    // if(mesh == nullptr){
-    //     std::cout << "Something bad happened...";
-    //     exit(1);
-    // }
-    // std::cout << "Done reading" << std::endl;
-    // compute_orientation(*mesh);
-    // auto volume = getVolume(*mesh);
     
-    // writeOFF("test.off", *mesh);
-
-    // std::cout << "Volume: " << volume << std::endl;
-    // //mesh->genGraph("test.dot");
-    // writeDOT("test.dot", *mesh);
-
-    // int trials = 10;
-    // std::chrono::duration<double> elapsed_seconds;
-
-    // for(int trial=0; trial < trials; ++trial){
-    //     std::chrono::time_point<std::chrono::system_clock> start, end;
-    //     start = std::chrono::system_clock::now();
-    //     // CODE GOES HERE
-
-    //     auto mesh = SurfaceMeshOld::readOFF(argv[1]);
-    //     for(int i=0; i < 10; ++i){
-    //         mesh->normalSmooth();
-    //     }
-
-    //     // CODE ENDS
-    //     end = std::chrono::system_clock::now();
-    //     free(mesh);
-    //     elapsed_seconds += end-start;
-    // }
-    // elapsed_seconds /= trials;
-    // std::cout << "Average time old: " << elapsed_seconds.count() << "s\n";
-    
-    // // New code benchmark 
-    // for(int trial=0; trial < trials; ++trial){
-    //     std::chrono::time_point<std::chrono::system_clock> start, end;
-    //     start = std::chrono::system_clock::now();
-    //     // CODE GOES HERE
-
-    //     auto mesh = readOFF(argv[1]);
-    //     if(mesh == nullptr){
-    //         std::cout << "Something bad happened...";
-    //         exit(1);
-    //     }
-
-    //     compute_orientation(*mesh);
-
-    //     for(auto i = 0; i < 1; ++i){
-    //         for(auto nid : mesh->get_level_id<1>())
-    //         {
-    //             normalSmooth(*mesh, nid);
-    //         }
-    //     }
-
-    //     // CODE ENDS
-    //     end = std::chrono::system_clock::now();
-    //     elapsed_seconds += end-start;
-    // }
-    // elapsed_seconds /= trials;
-    // std::cout << "Average time new: " << elapsed_seconds.count() << "s\n";
-
-
     std::cout << "EOF" << std::endl;
 }
