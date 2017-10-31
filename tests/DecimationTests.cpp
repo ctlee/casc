6b441b35aa11a954a70f52925027ad15cc28c972
ctlee <ctlee@ucsd.edu>
2017-10-31 15:45:49 -0700

    Cleanup of decimation tests

--- tests/DecimationTests.cpp
+++ tests/DecimationTests.cpp
@@ -59,33 +59,33 @@ protected:
 	using SimplexSet = typename casc::SimplexSet<SurfaceMeshType>;
 };
 
-template <typename T, std::size_t k>
-std::ostream& operator<<(std::ostream& out, const std::array<T,k>& A)
-{
-    out << "[";
-    for(int i = 0; i + 1 < k; ++i)
-    {
-        out << A[i] << " ";
-    }
-    if(k > 0)
-    {
-        out << A[k-1];
-    }
-    out << "]";
-    return out;
-}
+// template <typename T, std::size_t k>
+// std::ostream& operator<<(std::ostream& out, const std::array<T,k>& A)
+// {
+//     out << "[";
+//     for(int i = 0; i + 1 < k; ++i)
+//     {
+//         out << A[i] << " ";
+//     }
+//     if(k > 0)
+//     {
+//         out << A[k-1];
+//     }
+//     out << "]";
+//     return out;
+// }
 
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::set<T>& A)
-{
-    out << "[";
-    for(auto a : A)
-    {
-        std::cout << a << " ";
-    }
-    out << "]";
-    return out;
-}
+// template <typename T>
+// std::ostream& operator<<(std::ostream& out, const std::set<T>& A)
+// {
+//     out << "[";
+//     for(auto a : A)
+//     {
+//         std::cout << a << " ";
+//     }
+//     out << "]";
+//     return out;
+// }
 
 template <typename Complex>
 struct Callback
